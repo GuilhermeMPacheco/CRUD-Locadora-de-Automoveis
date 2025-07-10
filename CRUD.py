f9b6d2cb3d5241103c424f1cc35681a4bc8104ef
Guilherme Manoel Pacheco <guilhermempacheco2@gmail.com>
2025-07-09 21:24:41 -0300

    first commit

---
+++ CRUD.py
@@ -0,0 +1,148 @@
+import json
+from datetime import date
+import math
+
+# Arquivo para armazenar informações
+registros = "registros.json"
+produtos = "produtos.json"
+
+# Função para carregar dados do arquivo
+def carregar_dados(type):
+    if type == "clientes":
+        try:
+            with open(registros, "r") as arquivo: #Faz a leitura do arquivo .json
+                return json.load(arquivo)
+        except FileNotFoundError:
+            return []
+    elif type == "produtos":
+        try:
+            with open(produtos, "r") as arquivo: #Faz a leitura do arquivo .json
+                return json.load(arquivo)
+        except FileNotFoundError:
+            return []
+
+# Função para salvar dados no arquivo
+def salvar_dados(cadastros, type):
+    if(type == "clientes"):
+        with open(registros, "w") as arquivo: #Escreve o arquivo ou sobrescreve um arquivo .json
+            json.dump(cadastros, arquivo, indent=4)  #Descarrega as informações para cadastros
+    elif(type == "produtos"):
+        with open(produtos, "w") as arquivo: #Escreve o arquivo ou sobrescreve um arquivo .json
+            json.dump(cadastros, arquivo, indent=4)  #Descarrega as informações para cadastros
+
+# Função para adicionar um novo cadastro
+def adicionar_cadastro():
+    cadastros = carregar_dados("clientes")
+    nome_cliente = input("Digite o nome do cliente: ")
+    if nome_cliente.strip() == "":
+        print("Nome não pode estar vazio.")
+        return
+    try:
+        diaNascimento = int(input("Digite seu dia de nascimento: "))
+        mesNascimento = int(input("Digite a seu mês de nascimento: "))
+        anoNascimento = int(input("Digite a seu ano de nascimento: "))
+        dataNascimento = date(anoNascimento,mesNascimento,diaNascimento)
+        diferencaData = date.today() - dataNascimento
+        idade = math.floor(diferencaData.days/365)
+    except ValueError:
+        print("Use apenas número para as datas.")
+        return
+
+    email = input("Digite o email: ")
+    if "@" not in email:
+        print("E-mail inválido.")
+        return
+
+    cpf = input("Digite o CPF: (use apenas números) ")
+    if len(cpf) != 11 or not cpf.isdigit():
+        print("Cpf inválido.")
+        return
+
+    telefone = input("Digite um número para contato: (use apenas números) ")
+    if not telefone.isdigit():
+        print("Número de Telefone inválido")
+        return
+
+    cadastros.append({"nome": nome_cliente, "idade": idade, "email": email, "cpf": cpf, "telefone": telefone}) #Adiciona as informações nos vetores
+    salvar_dados(cadastros, "clientes")
+    print("\nCliente cadastrado com sucesso!")
+
+# Função para verificar os cadastros existentes
+def verificar_cadastros():
+    cadastros = carregar_dados("clientes")
+    if cadastros:
+        # Cabeçalho da tabela
+        print(f"\n{'Nome':<20} {'Idade':<6} {'Email':<30} {'CPF':<15} {'Telefone':<15}") #Formatação do cabeçalho
+        print("-" * 100)  # Linha divisória
+
+        # Exibição dos dados dos clientes
+        for cliente in cadastros:
+            cpfFormatado = f"{cliente['cpf'][:3]}.{cliente['cpf'][3:6]}.{cliente['cpf'][6:9]}-{cliente['cpf'][9:]}" # Formatação do cpf para o formato com . e - 
+            print(f"\n{cliente['nome']:<20} {cliente['idade']:<6} {cliente['email']:<30} {cpfFormatado:<15} {cliente['telefone']:<15}")#Formatação das váriaveis
+    else:
+        print("\nNenhum cadastro encontrado")
+
+# Função para atualizar um cadastro existente
+def atualizar_cadastros():
+    cadastros = carregar_dados("clientes")
+    verificar_cadastros() #Chama a função de verificação já formatada para a exibição dos cadastros
+    cpf = input("\nDigite o CPF do cliente que deseja atualizar (ou 'cancelar' para voltar): ")
+    if cpf.lower() == 'cancelar': #Utilização do lower para o uso da palavra em maiuscula como input.
+        print("\nOperação de atualização cancelada.")
+        return #Encerra o comando e retorna para o menu de escolha
+    for cliente in cadastros: #Verifica cada variavel dentro da lista
+        if cliente["cpf"] == cpf:
+                opcao = input("Digite a opção que deseja alterar: ")
+
+                if(opcao.lower() not in cliente): # Caso a opção escolhida não exista.
+                    print("Opção Inválida!") # Ele envia essa mensagem
+                    return
+                else: # Caso a opcao exista
+                    cliente[opcao.lower()] = input(F"Novo {opcao}: ") # Ele pede para atualizá-la               
+                    salvar_dados(cadastros, "clientes") # E Envia para o JSON
+                    print("\nCadastro atualizado com sucesso!")
+                return 
+    print("\nCadastro não localizado") # Caso não encontre o CPF, essa mensagem será enviada.
+
+# Função para excluir um cadastro
+def excluir_cadastro():
+    cadastros = carregar_dados("clientes")
+    verificar_cadastros()
+    cpf = input("\nDigite o CPF do cliente a ser excluído (ou 'cancelar' para voltar): ")
+    
+    if cpf.lower() == 'cancelar':
+        print("\nOperação de exclusão cancelada.")
+        return 
+    if not any(cliente["cpf"] == cpf for cliente in cadastros): #verifica se os valores são verdades, caso não, então não sera localizado
+        print("\nCadastro não localizado.")
+        return
+    cadastros = [cliente for cliente in cadastros if cliente["cpf"] != cpf] #Faz a verificação de cada cliente para fazer a exclusão criando uma lista sobre a outra (list comprehension)
+    salvar_dados(cadastros, "clientes")
+    print("\nCadastro excluído com sucesso!")
+
+# Função para exibir o menu e processar as escolhas do usuário
+def menu():
+    while True:
+        print("\n---BANCO DE DADOS DE CLIENTES---")
+        print("1. Fazer cadastro")
+        print("2. Consultar cadastro")
+        print("3. Atualizar cadastro")
+        print("4. Excluir cadastro")
+        print("5. Sair")
+
+        escolha = input("\nEscolha uma das opções: ") 
+        #Permite a escolha do menu
+        if escolha == "1":
+            adicionar_cadastro()
+        elif escolha == "2":
+            verificar_cadastros()
+        elif escolha == "3":
+            atualizar_cadastros()
+        elif escolha == "4":
+            excluir_cadastro()
+        elif escolha == "5":
+            print("\nFinalizando o programa...")
+            break
+        else:
+            print("\nEscolha inválida, por favor escolha uma opção disponível.")
+menu() #Chama o menu para exibição
